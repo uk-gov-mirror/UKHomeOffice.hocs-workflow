379cac447f473b1b4d15d564b718d66a53fc960d
Mark Wright <mark.wright@bjss.com>
2021-01-25 18:41:43 +0000

    HOCS-2296 : change to Official clears Ministerial data inc Escalated, fix

--- src/test/java/uk/gov/digital/ho/hocs/workflow/processes/MPAMTriageEscalate.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/processes/MPAMTriageEscalate.java
@@ -21,6 +21,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 @Deployment(resources = "processes/MPAM_TRIAGE_ESCALATE.bpmn")
@@ -42,6 +44,71 @@ public class MPAMTriageEscalate {
     @Before
     public void setup() {
         Mocks.register("bpmnService", bpmnService);
+    }
+
+    @Test
+    public void whenMinisterialChangedToOfficial_thenMinisterialValuesAreCleared() {
+
+        when(processScenario.waitsAtUserTask("Validate_UserInput"))
+                .thenReturn(task -> task.complete(withVariables(
+                        "DIRECTION", "UpdateRefType",
+                        "RefType", "Ministerial",
+                        "RefTypeCorrection", "Correction")));
+        when(processScenario.waitsAtUserTask("Validate_ReferenceTypeToOfficial"))
+                .thenReturn(task -> task.complete(withVariables(
+                        "DIRECTION", "BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables(
+                        "DIRECTION", "FORWARD",
+                        "valid", false)))
+                .thenReturn(task -> task.complete(withVariables(
+                        "DIRECTION", "FORWARD",
+                        "valid", true,
+                        "CaseNote_TriageChangeCaseType", "Casenote")));
+
+        Scenario.run(processScenario)
+                .startByKey("MPAM_TRIAGE_ESCALATE")
+                .execute();
+
+        verify(processScenario, times(3)).hasCompleted("Screen_ReferenceTypeToOfficial");
+        verify(processScenario).hasCompleted("Service_UpdateRefTypeToOfficial");
+        verify(bpmnService).updateValue(any(), any(), eq("RefType"), eq("Official"), eq("RefTypeStatus"), eq("Confirm"));
+        verify(processScenario).hasCompleted("Service_ClearMinisterialValues");
+        verify(bpmnService).blankCaseValues(any(), any(), eq("MinSignOffTeam"), eq("Addressee"));
+        verify(processScenario).hasCompleted("Service_SaveRefTypeChangeCaseNote");
+        verify(bpmnService).createCaseConversionNote(any(), any(), eq("Casenote"));
+        verify(processScenario).hasFinished("EndEvent_MpamTriageEscalate");
+    }
+
+    @Test
+    public void whenOfficialChangedToMinisterial_thenMinisterialValuesAreNotCleared() {
+
+        when(processScenario.waitsAtUserTask("Validate_UserInput"))
+                .thenReturn(task -> task.complete(withVariables(
+                        "DIRECTION", "UpdateRefType",
+                        "RefType", "Official",
+                        "RefTypeCorrection", "Correction")));
+        when(processScenario.waitsAtUserTask("Validate_ReferenceTypeToMinisterial"))
+                .thenReturn(task -> task.complete(withVariables(
+                        "DIRECTION", "BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables(
+                        "DIRECTION", "FORWARD",
+                        "valid", false)))
+                .thenReturn(task -> task.complete(withVariables(
+                        "DIRECTION", "FORWARD",
+                        "valid", true,
+                        "CaseNote_TriageChangeCaseType", "Casenote")));
+
+        Scenario.run(processScenario)
+                .startByKey("MPAM_TRIAGE_ESCALATE")
+                .execute();
+
+        verify(processScenario, times(3)).hasCompleted("Screen_ReferenceTypeToMinisterial");
+        verify(processScenario).hasCompleted("Service_UpdateRefTypeToMinisterial");
+        verify(bpmnService).updateValue(any(), any(), eq("RefType"), eq("Ministerial"), eq("RefTypeStatus"), eq("Confirm"));
+        verify(processScenario).hasCompleted("Service_SaveRefTypeChangeCaseNote");
+        verify(bpmnService).createCaseConversionNote(any(), any(), eq("Casenote"));
+        verify(processScenario).hasFinished("EndEvent_MpamTriageEscalate");
+        verify(bpmnService, never()).blankCaseValues(any(), any(), eq("MinSignOffTeam"), eq("Addressee"));
     }
 
     @Test
